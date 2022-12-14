e382da209b0b9798817e6a03f20f28d3a60ddb77
unknown <jerez4959@gmail.com>
2022-12-14 14:24:57 -0300

    controlgastos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <p className='footer'>Desarrollado con  {''}
+    
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="16" height="16" viewBox="0 0 24 24" stroke-width="3" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+    </svg>
+    
+   {''} por Carlos Jerez Flores</p>
+  )
+}
+
+export default Footer
