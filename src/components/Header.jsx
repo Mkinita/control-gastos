f924457375ef7a55076fbec7917cf0bb523ee631
unknown <jerez4959@gmail.com>
2022-12-14 14:57:30 -0300

    controlgastosoficialss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = ({
 }) => {
     return (
         <header>
-            <h1>Planificador de Gastos</h1>
+            <h1>Control de Gastos</h1>
 
             {isValidPresupuesto ? (
                 <ControlPresupuesto 
